828122f9739d1b66addd6e2ed2884d3161837271
bytemeprod <vavilov.main@icloud.com>
2025-09-27 12:25:01 +0300

    feat: implement sending message on frontend

--- frontend/scripts/script.js
+++ frontend/scripts/script.js
@@ -1,12 +1,26 @@
 const host = "ws://localhost:8080/ws";
+let conn;
+
+function sendMessage() {
+  var message = document.getElementById("message");
+  if (message != null) {
+    conn.send(message.value);
+  }
+  return false;
+}
 
 window.onload = function () {
-  let conn = new WebSocket(host);
+  document.getElementById("send_message_form").onsubmit = sendMessage;
+
+  conn = new WebSocket(host);
   conn.onopen = function (evt) {
     console.log("ws connected");
   };
   conn.onmessage = function (evt) {
-    console.log(`message: ${evt.data}`);
+    let messages_area = document.getElementById("messages_area");
+    let el = document.createElement("p");
+    el.textContent = evt.data;
+    messages_area.append(el);
   };
   conn.onerror = function (evt) {
     console.log("ws error occured");
