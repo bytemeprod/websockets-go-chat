4ba0036c75827417df04bc7689c4323200966e3d
bytemeprod <vavilov.main@icloud.com>
2025-09-27 12:56:27 +0300

    feat: connect ws handler to /ws path

--- cmd/websocket-go-chat/main.go
+++ cmd/websocket-go-chat/main.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"github.com/bytemeprod/websockets-go-chat/internal/config"
+	"github.com/bytemeprod/websockets-go-chat/internal/handlers/socket"
+	"github.com/bytemeprod/websockets-go-chat/internal/manager"
 	"github.com/labstack/echo"
 )
 
 func main() {
 	config := config.MustLoadConfig()
+	manager := manager.NewManager()
 	e := echo.New()
-	//e.Logger.SetLevel(log.OFF)
 	e.Static("/", "./frontend")
-	//e.GET("/ws", handlers.ServeWs)
+	e.GET("/ws", socket.NewHandler(manager))
 	e.Start(config.Host + ":" + config.Port)
 }
