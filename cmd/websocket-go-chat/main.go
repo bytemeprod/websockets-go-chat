195426b88d25370c6a513ae72302b38ab94c034e
bytemeprod <vavilov.main@icloud.com>
2025-10-01 12:37:20 +0300

    feat: initialize redis & rebuild handlers

--- cmd/websocket-go-chat/main.go
+++ cmd/websocket-go-chat/main.go
@@ -1,17 +1,34 @@
 package main
 
 import (
+	"context"
+	"log"
+
 	"github.com/bytemeprod/websockets-go-chat/internal/config"
+	"github.com/bytemeprod/websockets-go-chat/internal/handlers/login"
 	"github.com/bytemeprod/websockets-go-chat/internal/handlers/socket"
 	"github.com/bytemeprod/websockets-go-chat/internal/manager"
+	"github.com/bytemeprod/websockets-go-chat/internal/redisstore"
 	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 )
 
 func main() {
 	config := config.MustLoadConfig()
-	manager := manager.NewManager()
+	storage, err := redisstore.NewClient(config.RedisConfig.Addr, config.RedisConfig.Password)
+	if err != nil {
+		log.Fatal(err)
+	}
+	manager := manager.NewManager(storage)
+	ctx := context.Background()
+
 	e := echo.New()
-	e.Static("/", "./frontend")
+	e.Use(middleware.CORS())
+
 	e.GET("/ws", socket.NewHandler(manager))
+
+	e.Static("/", "./frontend")
+	e.POST("/login", login.NewHandler(ctx, storage))
+
 	e.Start(config.Host + ":" + config.Port)
 }
