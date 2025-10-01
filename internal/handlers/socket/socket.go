582ec402d5664c9fce333003468b801dc095256c
bytemeprod <vavilov.main@icloud.com>
2025-10-01 12:35:11 +0300

    feat: implement jwt logic in socket handler

--- internal/handlers/socket/socket.go
+++ internal/handlers/socket/socket.go
@@ -1,12 +1,21 @@
 package socket
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
 
 	"github.com/bytemeprod/websockets-go-chat/internal/client"
 	"github.com/bytemeprod/websockets-go-chat/internal/manager"
+	"github.com/bytemeprod/websockets-go-chat/internal/tokens"
 	"github.com/gorilla/websocket"
 	"github.com/labstack/echo"
+)
+
+// Errors
+var (
+	ErrInvalidToken = errors.New("invalid token")
 )
 
 var upgrader = websocket.Upgrader{
@@ -14,20 +23,53 @@ var upgrader = websocket.Upgrader{
 	WriteBufferSize: 1024,
 }
 
+type Response struct {
+	Type    string `json:"type"`
+	Message string `json:"message"`
+}
+
 func NewHandler(manager *manager.Manager) echo.HandlerFunc {
-	return func(с echo.Context) error {
-		conn, err := upgrader.Upgrade(с.Response(), с.Request(), nil)
+	return func(c echo.Context) error {
+		token := c.QueryParam("token")
+
+		conn, err := upgrader.Upgrade(c.Response(), c.Request(), nil)
 		if err != nil {
-			log.Printf("Failed to upgrade connection: %v", err)
+			log.Printf("Failed to upgrade connection: %s\n", err.Error())
 			return err
 		}
 
-		client := client.NewClient(manager, conn)
+		claims, err := tokens.ValidateJWT([]byte("secret-as-fuck"), token) // move key to config
+		if err != nil {
+			return sendClosingResponse(conn, "error", ErrInvalidToken.Error())
+		}
+
+		sub, err := claims.GetSubject()
+		if err != nil {
+			return sendClosingResponse(conn, "error", ErrInvalidToken.Error())
+		}
+
+		client := client.NewClient(sub, manager, conn)
 		manager.AddClient(client)
+
+		fmt.Printf("User %s connected\n", sub)
 
 		go client.ReadConnection()
 		go client.WriteConnection()
 
 		return nil
 	}
+}
+
+func sendClosingResponse(conn *websocket.Conn, messageType, message string) error {
+	response := Response{
+		Type:    messageType,
+		Message: message,
+	}
+	data, err := json.Marshal(response)
+	if err != nil {
+		log.Printf("Failed to marshal response: %s\n", err.Error())
+		return err
+	}
+	conn.WriteMessage(websocket.BinaryMessage, data)
+	return conn.Close()
 }
